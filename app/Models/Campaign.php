68511aec5bf144830d9fe936834b6367a9193705
midhunps <midhunps.0@gmail.com>
2024-02-15 20:02:46 +0530

    Corrected campaigns enabled_hospitals value format issue

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -9,9 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Campaign extends Model
 {
     protected $fillable = ["name"];
-    protected $casts = [
-        'enabled_hospitals' => 'array'
-    ];
+    // protected $casts = [
+    //     'enabled_hospitals' => 'array'
+    // ];
     use HasFactory;
 
     public function isEnabled(): Attribute
@@ -29,24 +29,37 @@ class Campaign extends Model
         return $query;
     }
 
+    public function enabledHospitals(): Attribute
+    {
+        return Attribute::make(
+            get: function ($val) {
+                return json_decode($val);
+            }
+        );
+    }
+
     public function enableHospital($hospitalId)
     {
-        $eArr = $this->enabled_hospitals ?? [];
+        $eArr = $this->enabled_hospitals;
+        info($eArr);
         if (!in_array($hospitalId, $eArr)) {
+            info('inside in_array');
             array_push($eArr, $hospitalId);
-            $this->enabled_hospitals = $eArr;
+            $this->enabled_hospitals = '['.implode(",",$eArr).']';
             $this->save();
         }
     }
 
     public function disableHospital($hospitalId)
     {
-        $eArr = $this->enabled_hospitals ?? [];
+        $eArr = $this->enabled_hospitals;
+        info($eArr);
         if (in_array($hospitalId, $eArr)) {
+            info('inside in_array');
             $eArr = array_filter($eArr, function ($h) use ($hospitalId) {
                 return $h != $hospitalId;
             });
-            $this->enabled_hospitals = $eArr;
+            $this->enabled_hospitals = '['.implode(",",$eArr).']';
             $this->save();
         }
     }
